2aac3f8b783acc20aca8480b5ddb6541b5819aa1
Deepesh <91532309+deepesh06-95@users.noreply.github.com>
2022-04-05 17:40:14 +0530

    Update dataStructures.js

--- dataStructures.js
+++ dataStructures.js
@@ -78,27 +78,27 @@ console.log(obj)
 // Output should be object: {a: 7, b: 9, c: 4}
 // ```
 
-// let string="a:2,b:3,c:4,a:5,b:6";
-// function strtosum(str)
-// {
-//     let arr=str.split(",");
-//     let obj={};
-//     for(let i=0;i<arr.length;i++)
-//     {
-//      let [chr,val]=arr[i].split(":");
-//      if(obj[chr]==undefined)
-//      {
-//         obj[chr]=+val;
-//      }
-//      else
-//      {
-//          let x=obj[chr];
-//          obj[chr]=x+(+val)
-//      }
-//     }
-//     return obj;
-// }
-// console.log(strtosum(string))
+let string="a:2,b:3,c:4,a:5,b:6";
+function strtosum(str)
+{
+    let arr=str.split(",");
+    let obj={};
+    for(let i=0;i<arr.length;i++)
+    {
+     let [chr,val]=arr[i].split(":");
+     if(obj[chr]==undefined)
+     {
+        obj[chr]=+val;
+     }
+     else
+     {
+         let x=obj[chr];
+         obj[chr]=x+(+val)
+     }
+    }
+    return obj;
+}
+console.log(strtosum(string))
 
 
 
@@ -108,38 +108,38 @@ console.log(obj)
 // // exp = "{[({})]}" should be valid
 // // but "{[(]} should be invalid
 // // ```
-// function checkparenthesis(str)
-// {
-// let stack=[];
-// let obj={")":"(","]":"[","}":"{"};
-// for(let i=0;i<str.length;i++)
-// {
-//     if(str[i]=="("||str[i]=="{"||str[i]=="[")
-//     {
-//         stack.push(str[i])
-//     }
-//     else if(str[i]==")"||str[i]=="}"||str[i]=="]")
-//     {
-//         let c=obj[str[i]];
-//         if(stack[stack.length-1]==c)
-//         {
-//             stack.pop()
-//         }
-//         else
-//         {
-//          return "invalid";
-//         }
-//     }
-// }
-// if(stack.length==0)
-// {
-//     return "valid";
-// }
-// else
-// {
-//     return "invalid";
-// }
-// }
-// console.log(checkparenthesis("{[({})]}"));
-// console.log(checkparenthesis("{[(]}"));
+function checkparenthesis(str)
+{
+let stack=[];
+let obj={")":"(","]":"[","}":"{"};
+for(let i=0;i<str.length;i++)
+{
+    if(str[i]=="("||str[i]=="{"||str[i]=="[")
+    {
+        stack.push(str[i])
+    }
+    else if(str[i]==")"||str[i]=="}"||str[i]=="]")
+    {
+        let c=obj[str[i]];
+        if(stack[stack.length-1]==c)
+        {
+            stack.pop()
+        }
+        else
+        {
+         return "invalid";
+        }
+    }
+}
+if(stack.length==0)
+{
+    return "valid";
+}
+else
+{
+    return "invalid";
+}
+}
+console.log(checkparenthesis("{[({})]}"));
+console.log(checkparenthesis("{[(]}"));
 
